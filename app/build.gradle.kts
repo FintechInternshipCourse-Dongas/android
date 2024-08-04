b5a4fb357cef223271b2793e6d6705817ef5f47c
nangeee <miniwoninuna@gmail.com>
2024-08-04 15:44:53 +0900

    주석 및 줄바꿈 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,12 +60,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
-
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.cardview)
     implementation(libs.material)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
