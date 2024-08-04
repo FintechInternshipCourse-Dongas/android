9a2bf0eb493d35e08e3c9cb9319c72b60fa6b29c
Hanbi <rlagksql219@naver.com>
2024-08-04 11:57:40 +0900

    기본 홈 화면 완료 및 네비게이션 화면 연결

---
+++ app/src/main/java/com/example/seureureuk/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.seureureuk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
