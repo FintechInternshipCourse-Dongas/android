34d9a1b9b4b63beeed60bd5c5b0556cd909384d4
nangeee <miniwoninuna@gmail.com>
2024-08-05 15:21:01 +0900

    결제 완료 화면 초기 구현

--- app/src/main/java/com/example/seureureuk/PaymentConfirmationActivity.kt
+++ app/src/main/java/com/example/seureureuk/PaymentConfirmationActivity.kt
@@ -1,24 +1,17 @@
 package com.example.seureureuk
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class PaymentQRCodeActivity: AppCompatActivity() {
+class PaymentConfirmationActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_payment_qrcode)
+        setContentView(R.layout.activity_payment_confirmation)
 
         val backButton = findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener {
             finish()
-        }
-
-        val QRCodeImage = findViewById<ImageView>(R.id.qr_code_image)
-        QRCodeImage.setOnClickListener {
-            val intent = Intent(this, PaymentConfirmationActivity::class.java)
-            startActivity(intent)
         }
     }
 }
