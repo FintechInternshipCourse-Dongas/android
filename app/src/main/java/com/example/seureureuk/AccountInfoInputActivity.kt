b94f8f85cb4b01f95b49f2babbf8c1bb466c0179
nangeee <miniwoninuna@gmail.com>
2024-08-06 03:15:44 +0900

    은행 선택 모달 창 구현 및 연결 완료

--- app/src/main/java/com/example/seureureuk/AccountInfoInputActivity.kt
+++ app/src/main/java/com/example/seureureuk/AccountInfoInputActivity.kt
@@ -2,12 +2,21 @@ package com.example.seureureuk
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class AccountInfoInputActivity: AppCompatActivity() {
+class AccountInfoInputActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_info_input)
+
+        val bankSelector = findViewById<TextView>(R.id.bank_selector)
+
+        bankSelector.setOnClickListener {
+            val bottomSheet = BankSelectionBottomSheet()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
 
         val backButton = findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener {
