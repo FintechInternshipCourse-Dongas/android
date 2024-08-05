650f2886fe0a06c1489d17fb18b3f784fc7eb78f
nangeee <miniwoninuna@gmail.com>
2024-08-06 04:35:23 +0900

    계좌 등록 완료 화면 초기 구현

--- app/src/main/java/com/example/seureureuk/AccountVerificationActivity.kt
+++ app/src/main/java/com/example/seureureuk/AccountVerificationActivity.kt
@@ -1,8 +1,10 @@
 package com.example.seureureuk
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -42,6 +44,12 @@ class AccountVerificationActivity : AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
+
+        val verificationButton = findViewById<Button>(R.id.verification_button)
+        verificationButton.setOnClickListener {
+            val intent = Intent(this, AccountRegistrationConfirmationActivity::class.java)
+            startActivity(intent)
+        }
 
         val backButton = findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener {
