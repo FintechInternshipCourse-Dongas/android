f7a59806cc795c9083f82848d978691222b818aa
nangeee <miniwoninuna@gmail.com>
2024-08-06 04:04:45 +0900

    계좌 인증번호 입력 화면 초기 구현

--- app/src/main/java/com/example/seureureuk/AccountInfoInputActivity.kt
+++ app/src/main/java/com/example/seureureuk/AccountInfoInputActivity.kt
@@ -1,6 +1,8 @@
 package com.example.seureureuk
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,12 @@ class AccountInfoInputActivity : AppCompatActivity() {
         bankSelector.setOnClickListener {
             val bottomSheet = BankSelectionBottomSheet()
             bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
+
+        val accountVerificationButton = findViewById<Button>(R.id.account_verification_button)
+        accountVerificationButton.setOnClickListener {
+            val intent = Intent(this, AccountVerificationActivity::class.java)
+            startActivity(intent)
         }
 
         val backButton = findViewById<ImageView>(R.id.back_button)
---
+++ app/src/main/java/com/example/seureureuk/AccountVerificationActivity.kt
@@ -0,0 +1,51 @@
+package com.example.seureureuk
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountVerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_verification)
+
+        val input1 = findViewById<EditText>(R.id.number_input_1)
+        val input2 = findViewById<EditText>(R.id.number_input_2)
+        val input3 = findViewById<EditText>(R.id.number_input_3)
+
+        input1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length == 1) input2.requestFocus()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        input2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length == 1) input3.requestFocus()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        input3.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
