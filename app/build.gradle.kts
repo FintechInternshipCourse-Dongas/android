e83a10b176ad2908809e64106c99ef7243d505d7
nangeee <miniwoninuna@gmail.com>
2024-08-11 02:55:17 +0900

    요청 받은 정산 내역에 동의하는 api 초기 연동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,4 +92,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
 }
